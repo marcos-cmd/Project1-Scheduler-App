d31e078c5b44987734a5d31d75d1488d0a70d77a
marvelousmarcos <marcos.joseph.garcia@gmail.com>
2020-08-22 14:08:00 -0700

    added modal to html

--- app.js
+++ app.js
@@ -1,4 +1,33 @@
-window.onload = sendApiRequest
+// Get the modal
+var modal = document.getElementById("gameModal");
+
+// Get the button that opens the modal
+var startBtn = document.getElementById("startGame");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+startBtn.onclick = function() {
+	//open the modal
+  modal.style.display = "block";
+  // call our function, sendApiRequest
+  sendApiRequest();
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
 
 async function sendApiRequest (){
 	let response = await fetch(`https://opentdb.com/api.php?amount=1&type=multiple`);
