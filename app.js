e02fddc1858385021347d6a36442909788c8f6dd
marvelousmarcos <marcos.joseph.garcia@gmail.com>
2020-08-22 11:37:42 -0700

    trivia game skeleton

--- app.js
+++ app.js
@@ -1,73 +1,25 @@
-// // const token = z0_8LZtVy6P7sLQ93VZrFQ;
+window.onload = sendApiRequest
 
-var app = document.getElementById('root');
-
-var container = document.createElement('div');
-container.setAttribute('class', 'container');
-
-app.appendChild(container);
-
-var data = JSON.stringify({
-  "token": "z0_8LZtVy6P7sLQ93VZrFQ",
-  "data": {
-    "nameFirst": "nameFirst",
-    "personAvatar": "personAvatar",
-    "jobTitle": "personTitle",
-    "nickName": "personNickname",
-    "_repeat": 9
-  }
-});
-
-var request = new XMLHttpRequest();
-
-request.onload = function(){
-  // Access the JSON response generated from the request
-  var fakeData = JSON.parse(this.response);
-
-  // Conduct status check. We're doing some stuff for valid requests and a customary message for an invalid request.
-  if (request.status >= 200 && request.status < 400) {
-    fakeData.forEach(companyRole => {
-	  // Create a div element with a class of "card"
-	  var card = document.createElement('div');
-	  card.setAttribute('class', 'card');
-
-	  // Create an img element, populating it using the fake avatar retrieved from FakeJSON
-	  var elem = document.createElement("img");
-	  elem.setAttribute("src", companyRole.personAvatar);
-	  elem.setAttribute("height", "180px");
-	  elem.setAttribute("width", "100%");
-	  elem.setAttribute("alt", "Profile Picture");
-
-	  // Create a h1 element, populating it using the fake first name retrieved from FakeJSON
-	  var h1 = document.createElement('h1');
-	  h1.textContent = companyRole.nameFirst;
-
-	  // Create a p element, populating it using the fake job title retrieved from FakeJSON
-	  var p1 = document.createElement('p1');
-	  p1.textContent = companyRole.jobTitle;
-
-	  // Create a p element, populating it using the fake nickname retrieved from FakeJSON
-	  var p2 = document.createElement('p2');
-	  p2.textContent = 'Also known as "' + companyRole.nickName + '"';
-
-	  // Create a line break element, to separate the two p elements
-	  var br = document.createElement("br");
-
-	  // Append the div class "card" element to the previously created div class "container" element
-	  container.appendChild(card);
-
-	  // Append the img, h1, and p elements previously created to the div class "card" element
-	  card.appendChild(elem);
-	  card.appendChild(h1);
-	  card.appendChild(p1);
-	  card.appendChild(br);
-	  card.appendChild(p2);
-    });
-  } else {
-    console.log('You shall not pass');
-  }
+async function sendApiRequest (){
+	let response = await fetch(`https://opentdb.com/api.php?amount=1&type=multiple`);
+	console.log(response)
+	let data = await response.json()
+	console.log(data)
+	useApiData(data)
 }
 
-request.open("POST", "https://app.fakejson.com/q");
-request.setRequestHeader("content-type", "application/json");
-request.send(data);
+function useApiData(data){
+	document.querySelector("#category").innerHTML = `Category: ${data.results[0].category}`
+	document.querySelector("#difficulty").innerHTML = `Difficulty: ${data.results[0].difficulty}`
+	document.querySelector("#question").innerHTML = `Question: ${data.results[0].question}`
+	document.querySelector("#answer1").innerHTML = data.results[0].correct_answer
+	document.querySelector("#answer2").innerHTML = data.results[0].incorrect_answers[0]
+	document.querySelector("#answer3").innerHTML = data.results[0].incorrect_answers[1]
+	document.querySelector("#answer4").innerHTML = data.results[0].incorrect_answers[2]
+}
+
+let correctButton = document.querySelector("answer1")
+
+correctButton.addEventListener("click", ()=> {
+	alert("Correct! Nice job.")
+})
